55498cd7d6cb95df34ec8372c0aee4f5f447e657
Russell Dennis <russell.dennis42@yahoo.com>
2021-04-26 01:54:07 -0700

    "npm install mocha"

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,29 +1,34 @@
 const { expect } = require("chai");
-const  polybius  = require("../src/polybius");
+const { polybius } = require("../src/polybius");
 
 describe("polybius", () => {
-    it("translates the letters i and j to 42 when encoding", () => {});
+  it("translates the letters i and j to 42 when encoding", () => {
     const actual = polybius("tests i and j");
-    const expected ="4451344434 42 113341 42";
+    const expected = "4451344434 42 113341 42";
     expect(actual).to.equal(expected);
+  });
 
-    it("translates 42 to (i/j) when decoding", () => {});
+  it("translates 42 to (i/j) when decoding", () => {
     const actual = polybius("4451344434 42", false);
-    const expected ="tests (i/j)";
+    const expected = "tests (i/j)";
     expect(actual).to.equal(expected);
+  });
 
-    it("ignores capital letters.", () => {});
+  it("ignores capital letters.", () => {
     const actual = polybius("TEsTS CapS");
-    const expected ="4451344434 31115334";
+    const expected = "4451344434 31115334";
     expect(actual).to.equal(expected);
+  });
 
-    it("maintains spaces in the message when encoding", () => {});
+  it("maintains spaces in the message when encoding", () => {
     const actual = polybius("tests spaces encoding");
     const expected = "4451344434 345311315134 5133314341423322";
     expect(actual).to.equal(expected);
+  });
 
-    it("maintains spaces in the message when decoding", () => {});
+  it("maintains spaces in the message when decoding", () => {
     const actual = polybius("4451344434 345311315134 4151314341423322", false);
-    const expected ="tests spaces decod(i/j)ng";
+    const expected = "tests spaces decod(i/j)ng";
     expect(actual).to.equal(expected);
+  });
 });
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,46 +1,56 @@
 const { expect } = require("chai");
-const substitution = require("../src/substitution");
+const { substitution } = require("../src/substitution");
 
 describe("substitution()", () => {
-    it("returns false if the given alphabet is greater than 26 characters long.", () => {
+  it("returns false if the given alphabet is greater than 26 characters long.", () => {
     const actual = substitution("too long", "plmoknijbuhvygctfxrdzeswaq@$*");
     const expected = false;
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("returns false if the given alphabet is less than 26 characters long.", () => {
+  it("returns false if the given alphabet is less than 26 characters long.", () => {
     const actual = substitution("too short", "plmoknijbuhvygctfxrdzes");
     const expected = false;
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("correctly translates the given phrase, based on the alphabet given to the function.", () => {
+  it("correctly translates the given phrase, based on the alphabet given to the function.", () => {
     const actual = substitution("message", "plmoknijbuhvygctfxrdzeswaq");
     const expected = "ykrrpik";
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("returns false if there are any duplicate characters in the given alphabet.", () => {
+  it("returns false if there are any duplicate characters in the given alphabet.", () => {
     const actual = substitution("message", "plmoknijbuhvygctfxrdzeswap");
-    const expected = false ;
+    const expected = false;
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("maintains spaces in the message when encoding", () => {
-    const actual = substitution("tests spaces when encoding", "plmoknijbuhvygctfxrdzeswaq");
+  it("maintains spaces in the message when encoding", () => {
+    const actual = substitution(
+      "tests spaces when encoding",
+      "plmoknijbuhvygctfxrdzeswaq"
+    );
     const expected = "dkrdr rtpmkr sjkg kgmcobgi";
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("maintains spaces in the message when decoding", () => {
-    const actual = substitution("dkrdr rtpmkr sjkg okmcobgi", "plmoknijbuhvygctfxrdzeswaq", false);
+  it("maintains spaces in the message when decoding", () => {
+    const actual = substitution(
+      "dkrdr rtpmkr sjkg okmcobgi",
+      "plmoknijbuhvygctfxrdzeswaq",
+      false
+    );
     const expected = "tests spaces when decoding";
     expect(actual).to.equal(expected);
-    });
+  });
 
-    it("ignores capital letters.", () => {
-    const actual = substitution("tests caps AaBbCc", "plmoknijbuhvygctfxrdzeswaq");
+  it("ignores capital letters.", () => {
+    const actual = substitution(
+      "tests caps AaBbCc",
+      "plmoknijbuhvygctfxrdzeswaq"
+    );
     const expected = "dkrdr mptr ppllmm";
     expect(actual).to.equal(expected);
-    });
+  });
 });
