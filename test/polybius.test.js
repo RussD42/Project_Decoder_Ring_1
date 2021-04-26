22781a5308e304ea18d130c1fa42dcade222f7b4
Russell Dennis <russell.dennis42@yahoo.com>
2021-04-26 03:19:41 -0700

    "test descriptions and less 'to.equal()' used in tests"

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -2,31 +2,31 @@ const { expect } = require("chai");
 const { polybius } = require("../src/polybius");
 
 describe("polybius", () => {
-  it("translates the letters i and j to 42 when encoding", () => {
+  it("should translate the letters i and j to 42 when encoding", () => {
     const actual = polybius("tests i and j");
     const expected = "4451344434 42 113341 42";
     expect(actual).to.equal(expected);
   });
 
-  it("translates 42 to (i/j) when decoding", () => {
+  it("should translate 42 to (i/j) when decoding", () => {
     const actual = polybius("4451344434 42", false);
     const expected = "tests (i/j)";
     expect(actual).to.equal(expected);
   });
 
-  it("ignores capital letters.", () => {
+  it("should ignore capital letters.", () => {
     const actual = polybius("TEsTS CapS");
     const expected = "4451344434 31115334";
     expect(actual).to.equal(expected);
   });
 
-  it("maintains spaces in the message when encoding", () => {
+  it("should maintain spaces in the message when encoding", () => {
     const actual = polybius("tests spaces encoding");
     const expected = "4451344434 345311315134 5133314341423322";
     expect(actual).to.equal(expected);
   });
 
-  it("maintains spaces in the message when decoding", () => {
+  it("should maintain spaces in the message when decoding", () => {
     const actual = polybius("4451344434 345311315134 4151314341423322", false);
     const expected = "tests spaces decod(i/j)ng";
     expect(actual).to.equal(expected);
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -2,31 +2,28 @@ const { expect } = require("chai");
 const { substitution } = require("../src/substitution");
 
 describe("substitution()", () => {
-  it("returns false if the given alphabet is greater than 26 characters long.", () => {
+  it("should return false if the given alphabet is greater than 26 characters long.", () => {
     const actual = substitution("too long", "plmoknijbuhvygctfxrdzeswaq@$*");
-    const expected = false;
-    expect(actual).to.equal(expected);
+    expect(actual).to.be.false;
   });
 
-  it("returns false if the given alphabet is less than 26 characters long.", () => {
+  it("should return false if the given alphabet is less than 26 characters long.", () => {
     const actual = substitution("too short", "plmoknijbuhvygctfxrdzes");
-    const expected = false;
-    expect(actual).to.equal(expected);
+    expect(actual).to.be.false;
   });
 
-  it("correctly translates the given phrase, based on the alphabet given to the function.", () => {
+  it("should correctly translate the given phrase, based on the alphabet given to the function.", () => {
     const actual = substitution("message", "plmoknijbuhvygctfxrdzeswaq");
     const expected = "ykrrpik";
     expect(actual).to.equal(expected);
   });
 
-  it("returns false if there are any duplicate characters in the given alphabet.", () => {
+  it("should return false if there are any duplicate characters in the given alphabet.", () => {
     const actual = substitution("message", "plmoknijbuhvygctfxrdzeswap");
-    const expected = false;
-    expect(actual).to.equal(expected);
+    expect(actual).to.be.false;
   });
 
-  it("maintains spaces in the message when encoding", () => {
+  it("should maintain spaces in the message when encoding", () => {
     const actual = substitution(
       "tests spaces when encoding",
       "plmoknijbuhvygctfxrdzeswaq"
@@ -35,7 +32,7 @@ describe("substitution()", () => {
     expect(actual).to.equal(expected);
   });
 
-  it("maintains spaces in the message when decoding", () => {
+  it("should maintain spaces in the message when decoding", () => {
     const actual = substitution(
       "dkrdr rtpmkr sjkg okmcobgi",
       "plmoknijbuhvygctfxrdzeswaq",
@@ -45,7 +42,7 @@ describe("substitution()", () => {
     expect(actual).to.equal(expected);
   });
 
-  it("ignores capital letters.", () => {
+  it("should ignore capital letters.", () => {
     const actual = substitution(
       "tests caps AaBbCc",
       "plmoknijbuhvygctfxrdzeswaq"
